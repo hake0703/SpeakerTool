0ea677dd4c3720883d30df04f0898c4d7107a86b
Keith Harder <harderkeith@gmail.com>
2018-05-06 09:34:03 -0500

    Add project files.

---
+++ Speaker/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Speaker
+{
+    public partial class Form1 : Form
+    {
+        private SpeechRecognitionEngine engine;
+        private SpeechSynthesizer synthesizer;
+
+        public void LoadSpeech()
+        {
+            try
+            {
+                engine = new SpeechRecognitionEngine();
+                engine.SetInputToDefaultAudioDevice();
+
+                engine.LoadGrammar(new DictationGrammar());
+
+                engine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(rec);
+
+                engine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rec(object s, SpeechRecognizedEventArgs e)
+        {
+            label1.Text = "Me: " + e.Result.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadSpeech();
+        }
+    }
+}
